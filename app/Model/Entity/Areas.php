9db6f465f7a2b2eaf0582cab95c148fae09a1558
Tarcisio Sapucaia <tarcisiosapucaia27@gmail.com>
2022-08-30 17:29:50 -0300

    mod-30-08

--- app/Model/Entity/Areas.php
+++ app/Model/Entity/Areas.php
@@ -5,10 +5,18 @@ namespace App\Model\Entity;
 require __DIR__.'/../../../includes/app.php';
 
 use \App\Model\Entity\Locais as EntityLocal;
+use \App\Session\Admin\Login;
 
+//VERIFICAR SE O USUÁRIO ESTÁ LOGADO
+$loged = Login::isLogged();
+
+//id DO USUÁRIO DA SESSÃO
+$id_user = $_SESSION['admin']['usuario']['id'];
+
+//id DO LOCAL
 $id = $_POST['id'];
-$id_user = $_POST['id_user'];
 
+//VAI NO BANCO E PEGA AS ÁREAS REFERENTES AO LOCAL PARA O USUÁRIO LOGADO
 $itens = EntityLocal::getAreaItens($id, $id_user);
 
 echo json_encode($itens);
